f2ed4f2843f76f2e9f39ffe82193728ee720123e
Legrendorg <Legrendorg@ukr.net>
2024-12-09 21:14:49 +0200

    Добавил: Класс - User (#4)
    - Добавил Класс User Без метадов.

---
+++ src/modules/User.ts
@@ -0,0 +1,9 @@
+export default class User {
+    name: string;
+    age: number;
+
+    constructor(userName: string, userAge: number) {
+        this.name = userName;
+        this.age = userAge;
+    }
+}
